5c2dd2f550773c7e565be0f51e9749cc2baae464
sadowb <90420943+sadowb@users.noreply.github.com>
2021-12-11 01:23:03 +0100

    printed hello world

--- test.c
+++ test.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 int main ()
 { 
-    printf("this is my first test with github it is so much fun");
+    printf("this is my first test with github it is so much fun\n");
+    printf("hello world\n");
     return 0;
+    
 
 }
