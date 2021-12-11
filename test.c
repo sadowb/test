9a71afa338f493bde03f5cb17c75a305bad05979
sadowb <90420943+sadowb@users.noreply.github.com>
2021-12-11 03:07:56 +0100

    i made more changes

--- test.c
+++ test.c
@@ -2,7 +2,7 @@
 int main ()
 { 
     printf("this is my second test i have so much fun\n");
-    printf("let go to sleep\n");
+    printf("let go to slee and this is the last push that i would do p\n");
     return 0;
     
 
